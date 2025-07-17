bd68d58f5a9dedffe7b043eb7871b1980dfeceed
andrea-corradetti <andreacorradetti28@gmail.com>
2025-07-17 10:39:05 +0200

    add jar-name arg

--- scripts/submit-job.sh
+++ scripts/submit-job.sh
@@ -8,7 +8,7 @@ echo "🚀 Submitting Spark job to Dataproc..."
 # Defaults from env
 CLUSTER_NAME="${CLUSTER:-}"
 JOB_ID=""
-
+JAR_NAME="${JAR_NAME:-imr.jar}"
 JAR_ARGS=()
 
 # Separate script args and jar args
@@ -27,9 +27,13 @@ while [[ $# -gt 0 ]]; do
       JOB_ID="$2"
       shift 2
       ;;
+    --jar-name)
+      JAR_NAME="$2"
+      shift 2
+      ;;
     *)
       echo "❌ Unknown option: $1"
-      echo "Usage: $0 [--cluster CLUSTER_NAME] [--id JOB_ID] [-- <JAR_ARGS>...]"
+      echo "Usage: $0 [--cluster CLUSTER_NAME] [--id JOB_ID] [--jar-name FILE] [-- <JAR_ARGS>...]"
       exit 1
       ;;
   esac
@@ -40,7 +44,6 @@ if [[ -z "$CLUSTER_NAME" ]]; then
   exit 1
 fi
 
-JAR_NAME="${JAR_NAME:-imr.jar}"
 JAR_PATH="${JAR_PATH:-"$BUCKET"/jars/"$JAR_NAME"}"
 INPUT_URI="${INPUT_URI:-"$BUCKET"/order_products.csv}"
 OUTPUT_URI="${OUTPUT_URI:-"$BUCKET"/out/${JOB_ID:-latest}}"
@@ -51,7 +54,6 @@ echo "📤 Output URI:       $OUTPUT_URI"
 echo "☁️ Cluster:          $CLUSTER_NAME"
 [[ -n "$JOB_ID" ]] && echo "🆔 Job ID:           $JOB_ID"
 [[ "${#JAR_ARGS[@]}" -gt 0 ]] && echo "📎 Jar Args:         ${JAR_ARGS[*]}"
-
 
 PROPERTIES=spark.hadoop.mapreduce.outputcommitter.factory.class=org.apache.hadoop.mapreduce.lib.output.DataprocFileOutputCommitterFactory
 PROPERTIES+=,spark.hadoop.mapreduce.fileoutputcommitter.marksuccessfuljobs=false
@@ -74,8 +76,7 @@ JAR_ARGS+=(
   --force-write
 )
 
-# Combine job args and jar args
-
+# Submit the job
 gcloud dataproc jobs submit spark "${ARGS[@]}" -- "${JAR_ARGS[@]}"
 
 echo "✅ Job submitted."
